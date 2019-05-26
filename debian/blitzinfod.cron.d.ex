8b8f21807f230150b92b6e68d471e86b351b3649
Robert Habermann <mail@rhab.de>
2019-05-26 13:41:34 +0200

    clean up

--- debian/blitzinfod.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the blitzinfod package
-#
-0 4	* * *	root	[ -x /usr/bin/blitzinfod_maintenance ] && /usr/bin/blitzinfod_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(blitzinfod):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="blitzinfod" command="/usr/bin/blitzinfod"
